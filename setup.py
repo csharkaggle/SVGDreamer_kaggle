bbe76f299a1a451fad8943c77148d0ad5a7c4ef5
csharkaggle <gauuud+kaggle@gmail.com>
2025-04-29 19:19:21 +0900

    fix(setup): correct package_data and update long_description and url

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 import os
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='svgdreamer',
     version="0.1.0",
     packages=[p for p in find_packages() if p.startswith('svgdreamer') or p.startswith('ImageReward') ],
-    package_data={'pytorch_svgrender': ['py.typed']},
+    package_data={'svgdreamer': ['py.typed']},
     install_requires=[
     ],
     python_requires=">=3.7",
     author='Ximing Xing, Juncheng Hu et al.',
     author_email='ximingxing@gmail.com',
     description='SVG Differentiable Rendering: Generating vector graphics using neural networks.',
-    long_description="",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     keywords=[
         'Artificial Intelligence',
@@ -20,7 +23,7 @@ setup(
         'Generative Models',
         'SVG Generation',
     ],
-    url='https://github.com/ximinng/PyTorch-SVGRender',
+    url='https://github.com/ximinng/SVGDreamer',
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
